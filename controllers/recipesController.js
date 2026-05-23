8274a4173ccab7794099cbadb83b82409858d357
Randi Umphrey <33576756+randiU@users.noreply.github.com>
2026-05-23 12:11:07 -0600

    Updated case issue with file path in recipesController.js.

--- controllers/recipesController.js
+++ controllers/recipesController.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const Recipe = require('../models/Recipe');
+const Recipe = require('../models/recipe');
 
 const getAllRecipes = async (req, res) => {
   try {
