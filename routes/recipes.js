06a2c3cbbc648ea72df953cf0d1f9e475c12e23d
Randi Umphrey <33576756+randiU@users.noreply.github.com>
2026-06-05 22:48:44 -0600

    Require login for recipe changes.

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,12 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const recipesController = require('../controllers/recipesController');
+const recipesController = require("../controllers/recipesController");
+const { isAuthenticated } = require("../middleware/authenticate");
 
-router.get('/', recipesController.getAllRecipes);
-router.get('/:id', recipesController.getRecipeById);
-router.post('/', recipesController.createRecipe);
-router.put('/:id', recipesController.updateRecipe);
-router.delete('/:id', recipesController.deleteRecipe);
+router.get("/", recipesController.getAllRecipes);
+router.get("/:id", recipesController.getRecipeById);
+
+router.post("/", isAuthenticated, recipesController.createRecipe);
+router.put("/:id", isAuthenticated, recipesController.updateRecipe);
+router.delete("/:id", isAuthenticated, recipesController.deleteRecipe);
 
 module.exports = router;
